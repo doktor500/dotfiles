f34fb34bd13fff8160ecd1c06407539fe4670bd7
David Molinero <doktor500@users.noreply.github.com>
2023-09-18 14:30:40 +0100

    Update .zshnvm.zsh

--- .zshnvm.zsh
+++ .zshnvm.zsh
@@ -1,3 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+
+export NVM_AUTO_USE=true
